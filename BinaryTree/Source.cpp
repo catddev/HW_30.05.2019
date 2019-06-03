d5e6981df678c0df033eab6ab9c37195ca1991db
dinara0708 <aikenova01@gmail.com>
2019-06-03 22:49:09 +0600

    -

--- BinaryTree/Source.cpp
+++ BinaryTree/Source.cpp
@@ -14,34 +14,34 @@ int main() {
 
 	t.print();
 
-	//cout << endl;
-	//cout << t.search(100) << endl;
-	//cout << t.search(12) << endl;
+	cout << endl;
+	cout << t.search(100) << endl;
+	cout << t.search(12) << endl;
 
-	//cout << t.search(7) << endl;
-	//cout << t.search(6) << endl;
-	//cout << t.search(4) << endl;
-	//cout << t.search(2) << endl;
+	cout << t.search(7) << endl;
+	cout << t.search(6) << endl;
+	cout << t.search(4) << endl;
+	cout << t.search(2) << endl;
 	cout << t.search(3) << endl;
-	//cout << t.search(5) << endl;
+	cout << t.search(5) << endl;
 
-	//cout << t.search(0) << endl;
-	//cout << t.search(1) << endl;
+	cout << t.search(0) << endl;
+	cout << t.search(1) << endl;
 
 	cout << endl << endl;
 	//returning pointer
 	cout << t.search_ptr(100) << endl;
-	//cout << t.search_ptr(12) << endl;
+	cout << t.search_ptr(12) << endl;
 
-	//cout << t.search_ptr(7) << endl;
-	//cout << t.search_ptr(6) << endl;
-	//cout << t.search_ptr(4) << endl;
-	//cout << t.search_ptr(2) << endl;
+	cout << t.search_ptr(7) << endl;
+	cout << t.search_ptr(6) << endl;
+	cout << t.search_ptr(4) << endl;
+	cout << t.search_ptr(2) << endl;
 	cout << t.search_ptr(3) << endl;
-	//cout << t.search_ptr(5) << endl;
+	cout << t.search_ptr(5) << endl;
 
 	cout << t.search_ptr(0) << endl;
-	//cout << t.search_ptr(1) << endl;
+	cout << t.search_ptr(1) << endl;
 
 
 	//t.del(7);
